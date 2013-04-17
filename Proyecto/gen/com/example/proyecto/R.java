4f12c5af1b32da31906a6c3bdd9a317a84118d2b
daalol <daalol87@gmail.com>
2013-04-17 22:54:11 +0200

    Subiendo refrescos.xml (Aun sin terminar)

--- Proyecto/gen/com/example/proyecto/R.java
+++ Proyecto/gen/com/example/proyecto/R.java
@@ -26,12 +26,12 @@ public final class R {
     }
     public static final class id {
         public static final int LstOpciones=0x7f07001e;
-        public static final int MnuOpc1=0x7f070031;
-        public static final int MnuOpc2=0x7f070032;
-        public static final int MnuOpc3=0x7f070033;
+        public static final int MnuOpc1=0x7f07003a;
+        public static final int MnuOpc2=0x7f07003b;
+        public static final int MnuOpc3=0x7f07003c;
         public static final int RelativeLayout1=0x7f070000;
-        public static final int TableLayout1=0x7f070023;
-        public static final int bolleria=0x7f070029;
+        public static final int TableLayout1=0x7f07002c;
+        public static final int bolleria=0x7f070032;
         public static final int bombon=0x7f070013;
         public static final int borrar=0x7f07001f;
         public static final int borrar_todo=0x7f070020;
@@ -39,33 +39,42 @@ public final class R {
         public static final int cafe_con_leche=0x7f07000a;
         public static final int cafe_solo=0x7f070005;
         public static final int cafe_tiempo=0x7f07000c;
-        public static final int cafes=0x7f070025;
+        public static final int cafes=0x7f07002e;
         public static final int cafes_aceptar=0x7f070019;
         public static final int cafes_borrar=0x7f07001b;
         public static final int cafes_cancelar=0x7f07001a;
         public static final int capuchinno=0x7f070015;
         public static final int carajillo=0x7f070010;
         public static final int carajillo_de=0x7f07000f;
-        public static final int combinados=0x7f07002b;
+        public static final int combinados=0x7f070034;
         public static final int cortado=0x7f070007;
         public static final int descripcion=0x7f070018;
         public static final int enviar_al_camarero=0x7f070021;
-        public static final int especialidades=0x7f07002c;
-        public static final int helados=0x7f070028;
-        public static final int refrescos=0x7f070026;
+        public static final int especialidades=0x7f070035;
+        public static final int helados=0x7f070031;
+        public static final int horchata=0x7f070026;
+        public static final int limon_granizado=0x7f070027;
+        public static final int refresco_cola=0x7f070023;
+        public static final int refresco_limon=0x7f070024;
+        public static final int refresco_naranja=0x7f070025;
+        public static final int refrescos=0x7f07002f;
+        public static final int refrescos_aceptar=0x7f070029;
+        public static final int refrescos_borrar=0x7f07002b;
+        public static final int refrescos_cancelar=0x7f07002a;
         public static final int relativeLayout1=0x7f070003;
         public static final int relativeLayout2=0x7f070008;
         public static final int relativeLayout3=0x7f07000d;
         public static final int relativeLayout4=0x7f070011;
         public static final int relativeLayout5=0x7f070016;
         public static final int seleccion_de_productos=0x7f07001d;
-        public static final int tableRow1=0x7f070024;
-        public static final int tableRow2=0x7f070027;
-        public static final int tableRow3=0x7f07002a;
-        public static final int tableRow4=0x7f07002d;
+        public static final int tableRow1=0x7f07002d;
+        public static final int tableRow2=0x7f070030;
+        public static final int tableRow3=0x7f070033;
+        public static final int tableRow4=0x7f070036;
+        public static final int tercio_cerveza=0x7f070028;
         public static final int textView1=0x7f070004;
         public static final int textView11=0x7f070022;
-        public static final int textView111=0x7f070030;
+        public static final int textView111=0x7f070039;
         public static final int textView2=0x7f070006;
         public static final int textView3=0x7f070009;
         public static final int textView4=0x7f07000b;
@@ -75,8 +84,8 @@ public final class R {
         public static final int textView8=0x7f070017;
         public static final int volverAcerca=0x7f070001;
         public static final int volverInstrucciones=0x7f07001c;
-        public static final int volver_no_ok=0x7f07002f;
-        public static final int volver_ok=0x7f07002e;
+        public static final int volver_no_ok=0x7f070038;
+        public static final int volver_ok=0x7f070037;
     }
     public static final class layout {
         public static final int acerca_de=0x7f030000;
@@ -84,7 +93,8 @@ public final class R {
         public static final int cafes=0x7f030002;
         public static final int instrucciones_de_uso=0x7f030003;
         public static final int menu_principal=0x7f030004;
-        public static final int seleccion_productos=0x7f030005;
+        public static final int refrescos=0x7f030005;
+        public static final int seleccion_productos=0x7f030006;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
